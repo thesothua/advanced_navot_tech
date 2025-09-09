bc3acfbcde7a93d821144d5847b052d5e4396f8f
thesothua <praveensuthar9553@gmail.com>
2025-09-10 00:51:42 +0530

    testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,10 +20,11 @@
 
 
     <!-- Google Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
+     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700,800,900,1000&display=swap"
-        rel="stylesheet">
+    {{-- <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">  --}}
+
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto&family=Source+Sans+Pro&family=Noto+Sans&family=Lato&display=swap" rel="stylesheet">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -82,7 +83,9 @@
 
         body {
             /* padding-top: 76px; */
-            font-family: 'Poppins', sans-serif;
+            /* font-family: "sans-serif"; */
+
+            font-family:  "Lato", sans-serif;
             color: var(--dark-color);
             overflow-x: hidden;
         }
